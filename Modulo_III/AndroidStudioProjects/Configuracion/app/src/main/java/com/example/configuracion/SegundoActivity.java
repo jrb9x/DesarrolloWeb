d7ac9cc2d8508d07fad9a69a7d6a8679ea0aa4a8
LAPTOP-GSMHONSB\jorge <jorge91rb@gmail.com>
2017-02-12 18:49:49 +0100

    Ejercicio Config Android

--- Modulo_III/AndroidStudioProjects/Configuracion/app/src/main/java/com/example/configuracion/SegundoActivity.java
+++ Modulo_III/AndroidStudioProjects/Configuracion/app/src/main/java/com/example/configuracion/SegundoActivity.java
@@ -3,32 +3,69 @@ package com.example.configuracion;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class SegundoActivity extends AppCompatActivity {
 
-    private Button privacidad, bateria, restaurar, btnOK, btnAtras;
+    private Button restaurar, btnOK, btnAtras;
+    private Switch privacidad;
+    private ToggleButton bateria;
+    private String botonPrivacidad = "desactivada", botonBateria = "desactivado";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
 
-        privacidad = (Button) findViewById(R.id.switch1);
-        bateria = (Button) findViewById(R.id.toggleButton);
+        privacidad = (Switch) findViewById(R.id.switch1);
+        bateria = (ToggleButton) findViewById(R.id.toggleButton);
         restaurar = (Button) findViewById(R.id.button2);
         btnOK = (Button) findViewById(R.id.BtnOK);
         btnAtras = (Button) findViewById(R.id.BtnAtras);
 
-        privacidad.setOnClickListener(new View.OnClickListener() {
+        privacidad.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    botonPrivacidad = "activada";
+                }
+                else{
+                    botonPrivacidad = "desactivada";
+                }
+            }
+        });
+
+        bateria.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    botonBateria = "activado";
+                }
+                else{
+                    botonBateria = "desactivado";
+                }
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(SegundoActivity.this, TercerActivity.class);
                 String nombreUsuario = getIntent().getStringExtra("nombreUsuario");
                 String email = getIntent().getStringExtra("email");
-                Intent intent = new Intent(SegundoActivity.this, TercerActivity.class);
+
+
                 intent.putExtra("nombreUsuario", nombreUsuario);
                 intent.putExtra("email", email);
+                intent.putExtra("privacidad", botonPrivacidad);
+                intent.putExtra("bateria", botonBateria);
+                startActivity(intent);
             }
         });
 
--- Modulo_III/AndroidStudioProjects/Configuracion/app/src/main/java/com/example/configuracion/TercerActivity.java
+++ Modulo_III/AndroidStudioProjects/Configuracion/app/src/main/java/com/example/configuracion/TercerActivity.java
@@ -1,13 +1,42 @@
 package com.example.configuracion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TercerActivity extends AppCompatActivity {
+
+    private Button inicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercer);
+
+        String nombreUsuario = getIntent().getStringExtra("nombreUsuario");
+        String email = getIntent().getStringExtra("email");
+        String privacidad = getIntent().getStringExtra("privacidad");
+        String bateria = getIntent().getStringExtra("bateria");
+
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView10 = (TextView) findViewById(R.id.textView10);
+        TextView textView11 = (TextView) findViewById(R.id.textView11);
+
+        textView4.setText("Bienvenid@ " + nombreUsuario);
+        textView10.setText(privacidad);
+        textView11.setText(bateria);
+
+        inicio = (Button) findViewById(R.id.button);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TercerActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
